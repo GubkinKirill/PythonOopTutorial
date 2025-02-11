e8396e9e26a42f95011a39da0aaebff4fed8fd2b
Kirill Gubkin <k.gubkin@list.ru>
2025-02-11 21:58:07 +0600

    lesson4

--- lesson4.py
+++ lesson4.py
@@ -9,17 +9,20 @@ __del__(self) - финализатор класса
 class Point:
     color = 'red'
     circle = 2
-    def __init__(self):
+    def __init__(self, x = 0, y = 0):
         print('вызов инит')
-        self.x = 0
-        self.y = 0
+        self.x = x
+        self.y = y
     
+    def __del__(self):
+        print('вызов деструктора ' + str(self))
+
     def set_coords(self, x: int, y: int):
         self.x = x
         self.y = y
 
     def get_coords(self):
         return(self.x, self.y)
-    
-pt = Point()
+
+pt = Point(1, 2)
 print(pt.__dict__)
